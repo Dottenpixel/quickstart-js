afc959cec692f815c00d6e955048ad81b79f632d
DouG Molidor <doug@discover.ly>
2019-01-15 03:24:43 -0800

    Update credentials.js

--- auth/chromextension/credentials.js
+++ auth/chromextension/credentials.js
@@ -1,9 +1,12 @@
 // TODO(DEVELOPER): Change the values below using values from the initialization snippet: Firebase Console > Overview > Add Firebase to your web app.
 // Initialize Firebase
 var config = {
-  apiKey: '<YOUR_API_KEY>',
-  databaseURL: '<YOUR_DATABASE_URL>',
-  storageBucket: '<YOUR_STORAGE_BUCKET_NAME>'
+  apiKey: "AIzaSyCmWJmhBIsJ8pHx_faAxun5XaMBeyZEFn4",
+  authDomain: "dly-ext-saved-contacts-dev.firebaseapp.com",
+  databaseURL: "https://dly-ext-saved-contacts-dev.firebaseio.com",
+  projectId: "dly-ext-saved-contacts-dev",
+  storageBucket: "dly-ext-saved-contacts-dev.appspot.com",
+  messagingSenderId: "999208686170"
 };
 firebase.initializeApp(config);
 
